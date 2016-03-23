6b33c4b2b580b58549c4428da74df268402c4f86
Diego-F-Aguirre <diego3884@gmail.com>
2016-03-23 17:34:57 -0600

    Facebook Login Setup

--- Devslpes_Showcase/ViewController.swift
+++ Devslpes_Showcase/ViewController.swift
@@ -7,14 +7,27 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction func fbButtonPressed(sender: UIButton!) {
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logInWithReadPermissions(["email"], fromViewController: self) { (facebookResult: FBSDKLoginManagerLoginResult!, facebookError: NSError!) in
+            if facebookError != nil {
+                print("FAcebook login failed. Error \(facebookError)")
+            } else {
+                let accessToken = FBSDKAccessToken.currentAccessToken().tokenString
+                print("successfully logged in with Facebook. \(accessToken)")
+            }
+        }
+    }
+    
 }
-
---
+++ Pods/Target Support Files/Pods-Devslpes_Showcase/Pods-Devslpes_Showcase-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Devslpes_ShowcaseVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Devslpes_ShowcaseVersionString[];
+
